74382704a1f15453f67f5a657edd4018fa214b9f
Ali Aliev <shamkir@gmail.com>
2016-02-22 03:55:00 +0400

    some changes in project structure

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ```
 django-admin startproject projectname --template=https://github.com/aliev/django-skeleton/archive/master.zip
+cd projectname
+virtualenv .env
+source .env/bin/activate
+pip install -r requirements.txt
 ```
--- src/conf/base.py
+++ src/conf/base.py
@@ -54,8 +54,8 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
