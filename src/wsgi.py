389bef9714e7456997d5522e895681d3d070aa9a
Ali Aliev <shamkir@gmail.com>
2016-03-24 01:33:12 +0400

    ups

--- src/wsgi.py
+++ src/wsgi.py
@@ -4,5 +4,7 @@ from sys import path
 
 path.insert(1, dirname(realpath(__file__)))
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.conf.local")
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
