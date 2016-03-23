44cd7300e2a280e9fc1fe694d5ed2dd8a92f5ac2
Ali Aliev <shamkir@gmail.com>
2016-03-24 00:17:25 +0400

    added static url, DEBUG=True by default, removed ALLOWED_HOSTS

--- src/conf/base.py
+++ src/conf/base.py
@@ -13,10 +13,7 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 SECRET_KEY = 'a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
+DEBUG = True
 
 # Application definition
 
--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('apps.core.urls', namespace='core')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', views.serve),
+    ]
+
