406f10eac0edb54b4926e628c1561426f7db285b
Ali Aliev <shamkir@gmail.com>
2016-04-25 14:03:36 +0500

    added django-debug-toolbar, added media url

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,3 +11,11 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
