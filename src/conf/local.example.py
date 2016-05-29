0c2ce476865dbe0f50d1d01b5e7cddc769cfd9e1
Ali Aliev <shamkir@gmail.com>
2016-05-30 00:44:27 +0400

    added postgresql database options for local.example.py

--- src/conf/local.example.py
+++ src/conf/local.example.py
@@ -1,3 +1,18 @@
 # CONSOLE EMAIL BACKEND FOR LOCAL DEVELOPMENT
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # END CONSOLE EMAIL BACKEND SETTINGS
+
+# POSTGRESQL DATABASE OPTIONS
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+"""
+# END POSTGRESQL DATABASE OPTIONS
