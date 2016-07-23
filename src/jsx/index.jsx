3f67ac8be82deb02f9373a8045726727e7a5a1a1
Ali Aliev <shamkir@gmail.com>
2016-07-23 19:09:50 +0400

    added jquery, font-awesome and how use it inside react

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,16 +1,19 @@
 require("../../node_modules/bootstrap/dist/css/bootstrap.min.css")
+require("../../node_modules/font-awesome/css/font-awesome.css")
+require("./main.css")
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class App extends React.Component {
-	render() {
-		return (
-			<div>
-        <p>This is a simple react component</p>
-				<p><a className="btn btn-primary btn-lg">Learn more</a></p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+      <p>This is a simple react component</p>
+      <p><a className="btn btn-primary btn-lg"><i className="fa fa-gitlab"></i>Learn more</a></p>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,24 @@ var path = require('path');
 var loaders = require('./webpack.loaders');
 
 module.exports = {
-	entry: [
-		'./src/jsx/index.jsx' // Your appʼs entry point
-	],
-	output: {
-		path: path.join(__dirname, 'src/static/js'),
-		filename: 'app.js'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	module: {
-		loaders: loaders
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		})
-	]
+  entry: [
+    './src/jsx/index.jsx' // Your appʼs entry point
+  ],
+  output: {
+    path: path.join(__dirname, 'src/static/bundle'),
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: loaders
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"production"'
+    }
+  })
+  ]
 };
